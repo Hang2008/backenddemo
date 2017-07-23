9c0067d8280340ce49d569f8e3efb411e72af640
WuYaoFangDa <692553423@qq.com>
2017-07-23 15:39:33 +0800

    8-16 done

--- application/api/model/ProductModel.php
+++ application/api/model/ProductModel.php
@@ -2,7 +2,11 @@
 
 namespace app\api\model;
 
-class ProductModel extends BannerModel {
+class ProductModel extends BaseModel {
     protected $table = 'product';
-    protected $hidden = ['delete_time', 'update_time', 'topic_img_id', 'head_img_id'];
+    protected $hidden = ['delete_time', 'update_time', 'create_time', 'pivot', 'from', 'category_id'];
+
+    protected function getMainImgUrlAttr($value, $data) {
+        return $this->handleImageUrl($value, $data);
+    }
 }
