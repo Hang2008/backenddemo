e2bb7ac3d32497d049fde6120eb05cf14222f771
WuYaoFangDa <692553423@qq.com>
2017-07-27 22:48:18 +0800

    9-10 done

--- application/route.php
+++ application/route.php
@@ -14,11 +14,21 @@
 use think\Route;
 
 Route::get('api/:version/banner/:id', 'api/:version.Banner/getBanner');
+
 Route::get('api/:version/theme', 'api/:version.Theme/getSimpleThemes');
 Route::get('api/:version/theme/:id', 'api/:version.Theme/getThemeWithProducts');
-Route::get('api/:version/product/category/:id', 'api/:version.Product/getByCategory');
-Route::get('api/:version/product/:id', 'api/:version.Product/getOneById', [], ['id' => '\d+']);
-Route::get('api/:version/product/recent', 'api/:version.Product/getRecentProduct');
+
+//Route::get('api/:version/product/category/:id', 'api/:version.Product/getByCategory');
+//Route::get('api/:version/product/:id', 'api/:version.Product/getOneById', [], ['id' => '\d+']);
+//Route::get('api/:version/product/recent', 'api/:version.Product/getRecentProduct');
+
+//路由分组
+Route::group('api/:version/product', function () {
+    Route::get('/category/:id', 'api/:version.Product/getByCategory');
+    Route::get('/:id', 'api/:version.Product/getOneById', [], ['id' => '\d+']);
+    Route::get('/recent', 'api/:version.Product/getRecentProduct');
+});
+
 Route::get('api/:version/category/all', 'api/:version.Category/getAllCategories');
 Route::post('api/:version/token/user', 'api/:version.Token/getToken');
 
