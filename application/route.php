18b575f5335fdbe36e5ddf736e030139f34c0be8
WuYaoFangDa <692553423@qq.com>
2017-07-27 22:40:01 +0800

    9-9 done

--- application/route.php
+++ application/route.php
@@ -16,9 +16,9 @@ use think\Route;
 Route::get('api/:version/banner/:id', 'api/:version.Banner/getBanner');
 Route::get('api/:version/theme', 'api/:version.Theme/getSimpleThemes');
 Route::get('api/:version/theme/:id', 'api/:version.Theme/getThemeWithProducts');
-Route::get('api/:version/product/recent', 'api/:version.Product/getRecentProduct');
 Route::get('api/:version/product/category/:id', 'api/:version.Product/getByCategory');
-Route::get('api/:version/product/:id', 'api/:version.Product/getOneById');
+Route::get('api/:version/product/:id', 'api/:version.Product/getOneById', [], ['id' => '\d+']);
+Route::get('api/:version/product/recent', 'api/:version.Product/getRecentProduct');
 Route::get('api/:version/category/all', 'api/:version.Category/getAllCategories');
 Route::post('api/:version/token/user', 'api/:version.Token/getToken');
 
