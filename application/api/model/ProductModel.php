731c019219ee50a34ccc9ba01796f53ca5c3884c
WuYaoFangDa <692553423@qq.com>
2017-07-28 20:34:20 +0800

    9-11 done

--- application/api/model/ProductModel.php
+++ application/api/model/ProductModel.php
@@ -11,6 +11,7 @@ class ProductModel extends BaseModel {
     }
 
     public static function getRecents($count) {
+        //对当前查询模型进行直接排序
         return self::limit($count)->order('create_time', 'desc')->select();
     }
 
@@ -21,8 +22,12 @@ class ProductModel extends BaseModel {
     }
 
     public static function getDetailById($id) {
-        //我写的
-        $product = self::with(['images', 'properties', 'images.imgUrl'])->find($id);
+        //连续链式with方法,可以针对每一步传递闭包方法
+//        $product = self::with(['images.imgUrl'])->with(['properties'])->find($id);
+        $product = self::with(['images' => function ($query) {
+            //这里可以针对关联模型进行排序
+            $query->with(['imgUrl'])->order('order', 'asc');
+        }])->with(['properties'])->find($id);
 //        var_dump($product);
         return $product;
     }
@@ -33,5 +38,7 @@ class ProductModel extends BaseModel {
 
     public function properties() {
         return $this->hasMany('PropertiesModel', 'product_id', 'id');
+        //是对当前模型进行排序, 所以要对关联模型进行排序不能这么写
+//        $this->order();
     }
 }
