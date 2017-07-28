ca99c12b0e11ab0e18391ee67a02ef9bbfa1444a
WuYaoFangDa <692553423@qq.com>
2017-07-28 22:27:05 +0800

    9-12 final done

---
+++ application/api/validate/AddressNew.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lihang
+ * Date: 28/07/2017
+ * Time: 9:14 PM
+ */
+
+namespace app\api\validate;
+
+
+class AddressNew extends BaseValidate {
+    protected $rule = [
+        'name' => 'require|isNotempty',
+        'mobile' => 'require|isMobile',
+        'province' => 'require|isNotempty',
+        'city' => 'require|isNotempty',
+        'country' => 'require|isNotempty',
+        'detail' => 'require|isNotempty'
+    ];
+}
